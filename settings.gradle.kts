d8c8fc145975d75a4077c8bf60c6e9b4194f04dc
Aled Barahoxha <aled.barahoxha@goupsrl.it>
2026-01-18 18:33:32 +0100

    fix(Maven): rimosso

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,6 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/registry")
-            credentials {
-                username = providers.gradleProperty("gpr.user").getOrElse(System.getenv("GITHUB_ACTOR"))
-                password = providers.gradleProperty("gpr.key").getOrElse(System.getenv("GITHUB_TOKEN"))
-            }
-        }
     }
 }
 
